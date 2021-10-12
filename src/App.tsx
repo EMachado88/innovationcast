4fafe53bb4e37c4250f037444cc8326517a6c88e
Emanuel Machado <emanuel@emachado.pt>
2021-10-12 11:21:46 +0100

    Implemented "new comment" modal

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,17 @@ export default function App() {
   const [posts, setPosts] = useState([])
   const [nextPage, setNextPage] = useState(0)
   const [prevPage, setPrevPage] = useState(0)
+  const [modalOpen, setModalOpen] = useState(false)
   const { page } = useQueryParams()
+
+  const handleToggleModal = () => {
+    setModalOpen(!modalOpen)
+  }
 
   useEffect(() => {
     async function fetchMyAPI() {
       const response = await fetch(`/api/posts?page=${page || 1}`)
       const json = await response.json()
-      console.log(json)
       setPosts(json.posts)
       setPrevPage(json.prev_page)
       setNextPage(json.next_page)
@@ -72,7 +76,7 @@ export default function App() {
         </div>
 
         <div className="column">
-          <button className="button is-info is-light mb-4">
+          <button onClick={handleToggleModal} className="button is-info is-light mb-4">
             <span className="icon-text">
               <span className="icon mr-2">
                 <i className="fas fa-regular fa-envelope"></i>
@@ -80,6 +84,25 @@ export default function App() {
             </span>
             <span>Comment</span>
           </button>
+
+          <div className={`modal${modalOpen ? ' is-active' : ''}`}>
+            <div className="modal-background" onClick={handleToggleModal}></div>
+
+            <div className="modal-card">
+              <header className="modal-card-head">
+                <p className="modal-card-title">New comment</p>
+              </header>
+              <section className="modal-card-body">
+              <textarea className="textarea" placeholder="Enter your comment..."></textarea>
+              </section>
+              <footer className="modal-card-foot">
+                <button className="button is-success">Submit</button>
+                <button onClick={handleToggleModal} className="button">Cancel</button>
+              </footer>
+            </div>
+
+            <button onClick={handleToggleModal} className="modal-close is-large" aria-label="close"></button>
+          </div>
         </div>
       </div>
     </div>
